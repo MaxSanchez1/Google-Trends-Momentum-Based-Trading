e20adb196904dd135204c8a47d2180ec087aef16
MaxSanchez1 <maxwsanchez@gmail.com>
2020-07-30 15:51:16 -0400

    Add more realistic signal that doesn't trade for too long

--- src/ticker_to_signal_df.py
+++ src/ticker_to_signal_df.py
@@ -4,21 +4,8 @@ import pandas_datareader as web
 from pytrends.request import TrendReq
 from datetime import timedelta
 from dateutil.relativedelta import *
+import numpy as np
 
-# method to create the dataframe of signals
-# - In: Ticker, Start date, End date
-# - Out: Df with index=date, most recent trend score,
-# rolling average volume (RAV), % change from prev RAV
-
-# signal (True if: trend over last by 2x, over RAV)
-
-# plan:
-# - most recent trend score: already done in sanity_check (with ffill)
-# - most recent trends score with no ffill so that pct_change works
-# - <'Trend_Change'>change from previous trend score: series.pct_change()
-# - rolling average volume --> df.rolling(window, min_periods)
-# - <'Pct_Over_Prev_Volume'>change from previous RAV --> series.pct_change()
-# - signal: apply(lambda row: ((row.Trend_Change > 200(%) and (row.volume > row.Pct_Over_Prev_Volume))
 
 # for testing
 pd.set_option('display.max_columns', None)
@@ -30,7 +17,7 @@ end_master = datetime.datetime(2020, 7, 20)
 # this is how to get only the date for filling in "timeframe" in pytr
 # print(start_master.strftime('%Y-%m-%d'))
 
-# maybe also take in time, default to all of 2019
+# Calculate a trading signal given the input data and some given parameters
 def tick_to_sig(company_name, ticker, start, end):
     # getting the data from yahoo finance for the given time period
     signal_df = web.DataReader(ticker, 'yahoo', start, end)
@@ -51,17 +38,13 @@ def tick_to_sig(company_name, ticker, start, end):
     name_plus_robinhood = str(company_name) + " stock"
     kw_list = [name_plus_robinhood]
     tf = start.strftime('%Y-%m-%d') + " " + end.strftime('%Y-%m-%d')
-    pytr.build_payload(kw_list, timeframe=tf)  # change this from being hardcoded once testing done
+    pytr.build_payload(kw_list, timeframe=tf)
     # df containing weekly interest score for the given keyword
     df = pytr.interest_over_time()
     # df = df.drop(columns=['isPartial'])
 
     # merging together the two dfs by using their dates
     signal_df = pd.merge(signal_df, df, how='outer', left_index=True, right_index=True)
-
-    # making a new column that fills in the weekday with the weekend's trend score so that there is a value
-    # for the signal to grab every day
-    # signal_df[[name_plus_robinhood + "_filled"]] = signal_df[[name_plus_robinhood]].fillna(method='ffill')
 
     # Calculate current Trend score's percent change over the previous one
     # signal_df['TrendChangePercent'] = signal_df[kw_list].pct_change().fillna(0) * 100
@@ -75,29 +58,39 @@ def tick_to_sig(company_name, ticker, start, end):
     # calculate the difference in cumulative maximum day to day to find outlier days where the local max went up a lot
     signal_df['LocalMaxChange'] = signal_df['LocalMax'].diff()
 
-    # second attempt at a signal
-    # looking for a day where the cumulative maximum has changed by some number
-    signal_df['Bool_Signal'] = signal_df.apply(
-        # will include the hertz pop but also the genius crash so we can't include it
+
+    # looking for a trend that is significant but isn't too high. We want to buy from here until the next peak or
+    # for some amount of days after where there is no peak.
+    # TODO: write something that counts the number of consecutive "TRUE"s and stop trading at a certain cutoff (10?)
+    signal_df['Bool_Temp'] = signal_df.apply(
         lambda row: (row.LocalMax > 20 and row.LocalMax < 55)
-                    and
+                    # and
                     # this is so we don't use the entire period until the next peak, just one day
                     # this strategy is much more volatile without this but it could be more profitable
-                    (row.LocalMaxChange > 0)
+                    #(row.LocalMaxChange > 0)
         , axis=1)
 
-    # return signal_df  # [['Volume', 'VolumeChangePercent']]
-    return signal_df  # [[name_plus_robinhood, 'TrendChangeRAW']]
-    # print(df.head(20))
-    # print(signal_df[kw_list].head(20))
-    # return signal_df[['Bool_Signal', 'hertz robinhood', 'TrendChangePercent_filled', 'RavChangePercent', 'RAV']]
-    # return signal_df['Bool_Signal']
+    # TESTING: column that figures out how many trues there are in a row in the bool signal column
+    signal_df['reset'] = signal_df.apply(
+        lambda row: 0 if row.Bool_Temp else 1, axis=1)
+    signal_df['val'] = signal_df.apply(
+        lambda row: 1 if row.Bool_Temp else 0, axis=1)
 
-    # first condition
-    # return signal_df.head(20)
-    # return signal_df[['hertz robinhood','TrendChangePercent', 'RavChangePercent', 'Bool_Signal']]
-#print(tick_to_sig("hertz","HTZ", start_master, end_master))
-# print(tick_to_sig("genius","GNUS"))
+    signal_df['cumsum'] = signal_df['reset'].cumsum()
+    signal_df['consecutive_days_true'] = signal_df.groupby(['cumsum'])['val'].cumsum()
+    signal_df = signal_df.drop(columns=['cumsum', 'reset', 'val'])
+
+    signal_df['Bool_Signal'] = signal_df.apply(
+        lambda row: True if (row.Bool_Temp and row.consecutive_days_true <= 20 and row.consecutive_days_true > 2) else False, axis=1)
+
+    return signal_df
+#
+# # TODO: when presenting, explain that this prediction could work for something like hertz because pop was more gradual
+# TODO:     whereas the genius pop happened too fast so that 36 hour delay was too much and the data was essentially useless
+# print(tick_to_sig("hertz","HTZ", start_master, end_master))
+# print(tick_to_sig("genius","GNUS", start_master, end_master))
+# print(tick_to_sig("tesla","TSLA", start_master, end_master))
+# print(tick_to_sig("aurora","ACB", start_master, end_master))
 
 
 # finding the specific ranges of positive signal examples to look at
@@ -117,10 +110,11 @@ sample_dict = {
 
 # method that takes in df of signals and tells you how much you'd make if you bought
 # at the close of the day of the signal and sold and the close of the next day
-def calculate_earnings_pct(company_name, ticker):
-    signal_df = tick_to_sig(company_name, ticker)
+def calculate_earnings_pct(company_name, ticker, start, end):
+    signal_df = tick_to_sig(company_name, ticker, start, end)
     #print(signal_df)
     signal_df_only_true = signal_df[signal_df.Bool_Signal]
+    # print(len(signal_df_only_true.index))
     # print(signal_df_only_true)
     # cumulative change in percent if the signal is followed
     percent_sum = 0
@@ -157,10 +151,11 @@ def calculate_earnings_pct(company_name, ticker):
         # print("Sold at:")
         # print(close_two_after_flag)
         percent_sum += float((close_two_after_flag - next_open) / next_open) * 100
+        # print(percent_sum)
     return percent_sum
 
-#print(calculate_earnings_pct("genius", "GNUS"))
-#print(calculate_earnings_pct("hertz", "HTZ"))
+# print(calculate_earnings_pct("genius", "GNUS", start_master, end_master))
+# print(calculate_earnings_pct("hertz", "HTZ", start_master, end_master))
 
 
 # mostly stocks from "most popular under $15" from robinhood
@@ -186,7 +181,7 @@ cheap_stocks = {
     "Sorrento": "SRNE",
     "macys": "M",
     # "": "",
-    "hertz": "HTZ",
+    # "hertz": "HTZ",
     "genius": "GNUS",
     "apple": "AAPL",
     "tesla": "TSLA",
@@ -202,23 +197,23 @@ cheap_stocks = {
 # method that takes in dictionary of company names as coloquially searched and
 # their corresponding ticker. Outputs the percentage returns as determined by
 # the signal generated by the chosen method
-def stock_to_result(dict_of_stocks):
+def stock_to_result(dict_of_stocks, start, end):
     # make a dataframe with the index being the names of the companies
     return_df = pd.DataFrame({'Company': list(dict_of_stocks.keys())})
     return_df['Signal Return'] = return_df.apply(
-        lambda row: calculate_earnings_pct(row.Company, dict_of_stocks[row.Company]), axis=1)
+        lambda row: calculate_earnings_pct(row.Company, dict_of_stocks[row.Company], start, end), axis=1)
     return return_df
 
-# result = stock_to_result(cheap_stocks)
-# print(result)
-# print("here is the average return on your money")
-# print(result['Signal Return'].mean())
+result = stock_to_result(cheap_stocks, start_master, end_master)
+print(result)
+print("here is the average return on your money")
+print(result['Signal Return'].mean())
 
 
 # forward-looking signal alert
 # - find today's date and subtract 6 months from it. That's the start and today is the end.
 # - plug that start and end into tick_to_sig
-# - if the bottom row (current day) is a True, this method will return true
+# - if the bottom row that we have historical data for (36 hours ago) is true, we buy and we hold until the next peak
 
 # TODO: current issue is that historical is 36 hours old so I'm going to have to make the best prediction
 # TODO:     I can with data that is a couple days old. Maybe now I'll try and do the more volatile strategy
@@ -233,7 +228,7 @@ def forward_looking_signal(company_name, ticker):
     print(signal_dataframe)
 
 
-print(forward_looking_signal("hertz", "HTZ"))
+# print(forward_looking_signal("hertz", "HTZ"))
 
 
 
